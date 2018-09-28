94eada450f41551ed2b9413d5b8a4b6928b0147a
Minamiciccc <june28munazo@gmail.com>
2018-09-28 15:41:46 -0700

    initial commit

---
+++ Memory/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by minami on 2018-09-28.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var buttons: [UIButton]!
+
+}
+
